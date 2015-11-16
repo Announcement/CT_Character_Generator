fb2e52743c73982581f6f51118c400d86b69242e
Jacob Francis Powers <xxv9xx@gmail.com>
2015-11-15 18:48:43 -0600

    Update test_module.rb

--- ideas/test_module.rb
+++ ideas/test_module.rb
@@ -1,33 +1,28 @@
 #!/usr/bin/env ruby
+#!/usr/bin/env ruby
 
 module Marine
-  def show_rank
-    puts "I am a Marine!"
-  end
+    def self.show_rank
+        puts "I am a Marine!"
+    end
 end
 
 module Pirate
-  def show_rank
-    puts "Arrgghhh!!"
-  end
+    def self.show_rank
+        puts "Arrgghhh!!"
+    end
 end
 
 class Character
-  include Marine
-  include Pirate
+    include Marine
+    include Pirate
 
-  def set_career(career)
-    puts career.instance_methods
-    career.show_rank
-  end
+    def set_career(career)
+        puts career.instance_methods
+        @career = career
+        career.show_rank
+    end
 end
 
 Fred = Character.new
 Fred.set_career(Marine)
-
-# Running this gets:
-# show_rank
-# ./test_module.rb:21:in `set_career': undefined method `show_rank' for Marine:Module (NoMethodError)
-#   from ./test_module.rb:26
-
-
